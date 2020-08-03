d9c8e0bc11da2b765a59f7b7841b0eca93135b06
Laura THAY <laura.thay@ipag.fr>
2020-08-03 19:03:34 +0200

    exercice 1 corrigé

---
+++ 1er jour/src/views/exo-1/Exercice1-part2.vue
@@ -0,0 +1,30 @@
+template>
+  <div class="exercise-1">
+    <!-- ## Templating -->
+    <h1>Bienvenue, {{ firstname + " " + lastname}} !</h1>
+    <h1>Bienvenue, {{ firstname }} {{ lastname }} !</h1>
+    <!-- ## Data Binding -->
+    <label for="">Mon animal totem est : {{animalTotem}}</label><br>
+    <input type="text" v-model="animalTotem"/><br><br>
+    <!--## Conditionnal Templating-->
+    <p v-if="age && age >= 0">J'ai {{age}} an(s) et je suis née en {{2020 - age}}</p>
+    <input type="number" v-model="age"/>
+    <!-- parseInt(variable) string > int ou variable.toString() int > string -->
+    <p v-if="parseInt(age) > 18">Youpi ! Je peux conduire !</p>
+    <p v-else-if="parseInt(age) === 18">C'est le grand jour, je passe mon permis.</p>
+    <p v-else-if="parseInt(age) < 18">Je peux passer mon permis dans {{18 - age}} année(s)</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "Exercise1",
+  data() {
+    return{
+      firstname: "Anne",
+      lastname: "Trotreau",
+      animalTotem: "",
+      age: ""
+    }
+  }
+};
+</script>
--- 1er jour/src/views/exo-1/Exercice1.vue
+++ 1er jour/src/views/exo-1/Exercice1.vue
@@ -1,14 +1,25 @@
 <template>
+<div>
   <div class="exercise-1">
-    <h1>Bienvenue, inconnu.e !</h1>
+    <input type="text" v-model="firstname" />
+    <input type="text" v-model="lastname" />
   </div>
+  <p v-if="firstname || lastname">Bienvenue, {{ firstname }}  {{ lastname }} !</p>
+ </div>
 </template>
 
 <script>
 export default {
-  name: "Exercise1"
+  name: 'Exercise1',
+
+  data() {
+    return {
+      firstname: "",
+      lastname: "",
+    };
+  },
 };
-</script>
+</script> 
 
 <style lang="stylus" scoped>
 @import '../../theme.styl';
